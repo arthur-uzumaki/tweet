b244ab78bc0742f91062594f1a7a9523b7d00564
Arthur Sousa <102182959+Arttanjeiro@users.noreply.github.com>
2023-03-22 22:42:17 -0300

    Salvando novo tweet com estado do react e colocando onKeyDom pra enviar novo tweet

--- src/pages/Status.tsx
+++ src/pages/Status.tsx
@@ -1,39 +1,80 @@
 
+import { FormEvent, KeyboardEvent, useState } from 'react'
 import { Header } from '../components/header/Header'
 import { Separator } from '../components/separator/Separator'
 import { Tweet } from '../components/tweet/Tweet'
 import './Status.css'
 
-const answers =[
-  'Concordo...',
-  'Olha, faz sentido!',
-  'Parabéns pelo seu progresso.'
-]
 
-export function Status(){
+/*
+* Fluxo de  renderização
+*
+* 1. Toda vez que alteramos o estado de um componente, TODO componente é recalculado.
+* 2. Toda vez que o seu componente PAI renderizar.
+* 3. Toda vez que que alguma das suas propriedades mudarem.
 
-  return(
+*/
+
+/**
+ * Algoritimo de reconciliação
+ * 
+ * 1. Cria em memória a nova versão do HTML do componente
+ * 2. Compara essa nova versão com a versão anterior do HTML (Diff)
+ * 3. Aplicar as operações JavaScript para alterar somente o necesário no HTML
+ * 
+ */
+
+export function Status() {
+
+
+  const [newAnswers, setNewAnswers] = useState('')
+  const [answers, setAnswers] = useState([
+    'Concordo...',
+    'Olha, faz sentido!',
+    'Parabéns pelo seu progresso.'
+
+  ])
+  function createNewAnswers(event: FormEvent) {
+    event.preventDefault()
+
+
+    setAnswers([newAnswers, ...answers])
+    setNewAnswers('')
+  }
+
+  function handleHotkeySubmit(event: KeyboardEvent){
+    if(event.key === 'Enter' && (event.ctrlKey || event.metaKey)){
+      setAnswers([newAnswers, ...answers])
+      setNewAnswers('')
+    }
+  }
+
+  return (
     <main className='status'>
 
-    <Header title='Tweet' />
-    <Tweet content='
+      <Header title='Tweet' />
+      <Tweet content='
     Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem asperiores laudantium cumque autem ex, facilis nobis mollitia unde nostrum eum veritatis accusantium obcaecati labore voluptatibus ab debitis eveniet error non.'/>
 
-    <Separator />
-    <form className='answer-tweet-form'>
-      <label htmlFor="tweet">
-        <img src="https://github.com/Arttanjeiro.png" alt="Arthur Sousa" />
-        <textarea id='tweet' placeholder="Tweet your answer? " />
-      </label>
+      <Separator />
+      <form onSubmit={createNewAnswers} className='answer-tweet-form'>
+        <label htmlFor="tweet">
+          <img src="https://github.com/Arttanjeiro.png" alt="Arthur Sousa" />
+          <textarea id='tweet'
+            value={newAnswers}
+            onChange={(e) => setNewAnswers(e.target.value)}
+            onKeyDown={handleHotkeySubmit}
+            placeholder="Tweet your answer? " />
+        </label>
 
-      <button type='submit'> Answer</button>
-    </form>
+        <button type='submit'> Answer</button>
+      </form>
 
 
-    {answers.map(answer =>{
-     return <Tweet key={answer} content={answer}/>
-    })}
+      {answers.map(answer => {
+        return <Tweet key={answer} content={answer} />
+      })}
 
-  </main>
+    </main>
   )
 }
--- src/pages/Timeline.tsx
+++ src/pages/Timeline.tsx
@@ -1,35 +1,64 @@
+import { FormEvent, KeyboardEvent, useState } from "react"
 import { Header } from "../components/header/Header"
 import { Separator } from "../components/separator/Separator"
 import { Tweet } from "../components/tweet/Tweet"
 import './Timeline.css'
 
-const tweets = [
-  'Meu primeiro tweets',
-  'Teste',
-  'Deu certo tweetar!',
-  
-]
-export function Timeline(){
-  return(
+
+export function Timeline() {
+
+  const [newTweet, setNewTweet] = useState('')
+  const [tweets, setTweets] = useState([
+    'Meu primeiro tweets',
+    'Teste',
+    'Deu certo tweetar!',
+
+  ])
+
+  function createNewTweet(event: FormEvent) {
+    event.preventDefault()
+
+    // tweets.push(newTweet)
+    setTweets([newTweet, ...tweets])
+    setNewTweet('')
+
+  }
+
+  function handleHotkeySubmit(event: KeyboardEvent) {
+    if (event.key === 'Enter' && (event.ctrlKey || event.metaKey)) {
+     
+      setTweets([newTweet, ...tweets])
+      setNewTweet('')
+
+    }
+  }
+
+  return (
     <main className='timeline'>
 
-          <Header title='Home' />
+      <Header title='Home' />
 
-          <form className='new-tweet-form'>
-            <label htmlFor="tweet">
-              <img src="https://github.com/Arttanjeiro.png" alt="Arthur Sousa" />
-              <textarea id='tweet' placeholder="What's happening? " />
-            </label>
+      <form onSubmit={createNewTweet} className='new-tweet-form'>
+        <label htmlFor="tweet" >
+          <img src="https://github.com/Arttanjeiro.png" alt="Arthur Sousa" />
+          <textarea id='tweet'
+            value={newTweet}
+            placeholder="What's happening? "
+            onChange={(e) => setNewTweet(e.target.value)}
+            onKeyDown={handleHotkeySubmit}
+          />
 
-            <button type='submit'> Tweet</button>
-          </form>
+        </label>
 
-          <Separator />
+        <button type='submit'> Tweet</button>
+      </form>
 
-          {tweets.map(tweet =>{
-           return <Tweet key={tweet} content={tweet}/>
-          })}
+      <Separator />
 
-        </main>
+      {tweets.map(tweet => {
+        return <Tweet key={tweet} content={tweet} />
+      })}
+
+    </main>
   )
 }
